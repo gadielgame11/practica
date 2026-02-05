14ae1ca09f7cb71153daf60afaa3e58f8d10228a
Jhon <hantoni577@gmail.com>
2026-02-05 17:26:02 -0500

    se agrega la parte de calcular calorias

--- plantilla.py
+++ plantilla.py
@@ -2,6 +2,20 @@ from menu import mostrar_menu
 from tmb import calcular_tmb
 from imc import calcular_imc
 
+def calcular_IMC(peso, altura):
+    return peso / (altura * altura)
+
+
+def calcular_porcentaje_grasa(peso, altura, edad, valor_genero):
+    imc = calcular_IMC(peso, altura)
+    return (1.2 * imc) + (0.23 * edad) - (10.8 * valor_genero) - 5.4
+
+
+def calcular_calorias_en_reposo(peso, altura, edad, valor_genero):
+    altura_cm = altura * 100
+    return (10 * peso) + (6.25 * altura_cm) - (5 * edad) + valor_genero
+
+
 def main():
     while True:
         mostrar_menu()
@@ -19,3 +33,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
