fff07ed6c2959d121aa0587ea9a2da3af2bee6c3
Alejandro <stivenst1504@gmail.com>
2026-02-05 17:46:34 -0500

    Menu

---
+++ menu.py
@@ -0,0 +1,7 @@
+def mostrar_menu():
+    print("================================")
+    print(" CALCULADORA METABÓLICA BASAL ")
+    print("================================")
+    print("1. Calcular IMC")
+    print("2. Calcular TMB")
+    print("3. Salir")
